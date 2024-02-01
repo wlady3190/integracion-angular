6c15886aa7dafa72f044350e19343a400f449ce5
wlady3190 <wlady_3190@hotmail.com>
2024-02-01 10:02:06 -0500

    generando componente estudiante y base de datos

---
+++ src/app/estudiantes/estudiantes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-estudiantes',
+  standalone: true,
+  imports: [],
+  templateUrl: './estudiantes.component.html',
+  styleUrl: './estudiantes.component.css'
+})
+export class EstudiantesComponent {
+
+}
